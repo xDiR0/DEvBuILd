67cc6e4366a2d70d1deed8078bf7a2ad80e0e8e3
soda0704 <denserbinlol@gmail.com>
2026-02-19 21:31:05 +0300

    update
    add SCREEN, SOUND menu.

--- menu/menu_settings/settings.gd
+++ menu/menu_settings/settings.gd
@@ -1,31 +1,35 @@
 extends Control
 
+@onready var settings: VBoxContainer = $SETTINGS
+@onready var screen: Panel = $SCREEN
+@onready var sound: Panel = $SOUND
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	settings.visible = true
+	screen.visible = false
+	sound.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu/main_menu/main_menu.tscn")
 
 
+
+
 func _on_sound_pressed() -> void:
-	pass # Replace with function body.
-
-
+	settings.visible = false
+	sound.visible = true
 func _on_control_pressed() -> void:
-	pass # Replace with function body.
-
-
+	pass
 func _on_graphics_pressed() -> void:
-	pass # Replace with function body.
-
-
+	pass
 func _on_screen_pressed() -> void:
-	pass # Replace with function body.
+	settings.visible = false
+	screen.visible = true
+
+func _on_back_options_pressed() -> void:
+	_ready()
