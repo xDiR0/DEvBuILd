2998889cdd5607fb3b431b1bb215f865480037ea
soda0704 <denserbinlol@gmail.com>
2026-04-11 21:55:59 +0300

    Update
    Add chest anim

--- WORLD/game/room2/chest.gd
+++ WORLD/game/room2/chest.gd
@@ -1,26 +1,24 @@
-extends Node3D
-
-@onready var activation_zone: Area3D = $ActivationZone
-@onready var door_pivot = $DoorPivot
-@onready var door_animations: AnimationPlayer = $DoorPivot/Door_animations
+extends StaticBody3D
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+@onready var activate_zone: Area3D = $"../ActivateZone"
 
 var player_in_zone = false
 var is_open = false
 var is_animating = false
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Подключаю зону активации
-	if activation_zone:
-		activation_zone.body_entered.connect(_on_player_entered)
-		activation_zone.body_exited.connect(_on_player_exited)
+	if activate_zone:
+		activate_zone.body_entered.connect(_on_player_entered)
+		activate_zone.body_exited.connect(_on_player_exited)
 	# Подключаю VoiceManager
 	var vm = get_node_or_null("/root/VoiceManager")
 	if vm:
 		vm.voice_command_detected.connect(_on_voice_command)
 	# Подключаю сигнал завершения анимации
-	if door_animations:
-		door_animations.animation_finished.connect(_on_animation_finished)
-
+	if animation_player:
+		animation_player.animation_finished.connect(_on_animation_finished)
 # Проверка входа в зону
 func _on_player_entered(body: Node):
 	if body.name == "Player":
@@ -31,28 +29,22 @@ func _on_player_exited(body: Node):
 	if body.name == "Player":
 		player_in_zone = false
 		print("Подойди ближе...")
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 # Получение команды
 func _on_voice_command(cmd: String):
-	# Проверка в зоне ли игрок
 	if not player_in_zone:
 		print("Ещё ближе...")
 		return
 	
-	# Проверка на анимацию двери
 	if is_animating:
 		return
 	
-	# Выполняем команду
 	if cmd == "open" and not is_open:
 		is_animating = true
-		door_animations.play("open")
-	elif cmd == "close" and is_open:
-		is_animating = true
-		door_animations.play("close")
+		animation_player.play("Chest_room2")
 
 # Когда анимация закончилась
 func _on_animation_finished(anim_name: String):
 	is_animating = false
-	if anim_name == "open":
+	if anim_name == "Chest_room2":
 		is_open = true
